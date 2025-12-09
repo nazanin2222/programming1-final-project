e736d4bf4644e2772458fc7557f4456ef2910324
nazanin <nazanineliasy@gmail.com>
2025-12-08 20:19:48 -0500

    commit

--- src/main/java/org/example/Student.java
+++ src/main/java/org/example/Student.java
@@ -9,9 +9,19 @@ public class Student {
     private Gender gender;
     private Address address;
     private Department department;
+    private List<Course> registeredCourses;
     private static int nextId = 1;
 
     public enum Gender {
         MALE, FEMALE
+    }
+
+    public Student(String studentName, Gender gender, Address address, Department department) {
+        this.studentId = String.format("S%05d", nextId++);
+        this.studentName = studentName;
+        this.gender = gender;
+        this.address = address;
+        this.department = department;
+        this.registeredCourses = new ArrayList<>(); // initialize ArrayList
     }
 }
