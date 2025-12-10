19a9c6fe7bf2302d652d407aa1ee8ac2d20d7a0e
nazanin <nazanineliasy@gmail.com>
2025-12-10 16:09:40 -0500

    commit

---
+++ src/main/java/org/example/Course.java
@@ -0,0 +1,29 @@
+package org.example;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+public class Course {
+    private final String courseId;
+    private String courseName;
+    private double credits;
+    private Department department;
+    private List<Assignment> assignments;
+    private List<Student> registeredStudents;
+    private static int nextId = 1;
+
+    public Course(String courseName, double credits, Department department) {
+        this.courseName = Util.toTitleCase(courseName);
+        this.credits = credits;
+        this.department = department;
+        this.assignments = new ArrayList<>();
+        this.registeredStudents = new ArrayList<>();
+        this.courseId = String.format("C-%s-%02d",
+                department != null ? department.getDepartmentId() : "D00", nextId++);
+    }
+}
